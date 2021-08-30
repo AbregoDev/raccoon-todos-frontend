db1eebb5923efdc2ba64bd190cdd5b0c25174f76
Bryan Moreno <abre.go@outlook.com>
2021-08-30 11:00:08 -0500

    delete components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
-import { AddComponent } from './add/add.component';
-import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent,
-    AddComponent,
-    EditComponent
   ],
   imports: [
     BrowserModule,
