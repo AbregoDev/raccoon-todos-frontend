cd029da8db0b52f30af972e3db289cf72536aae0
Bryan Moreno <abre.go@outlook.com>
2021-08-30 16:06:12 -0500

    CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,18 @@ import { TodoListService } from './services/todo-list.service';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
     completedTasks: ToDo[] = [];
     todoTasks: ToDo[] = [];
+
+    newTodoBody: string = '';
+
+    editedTask: ToDo = { id: '', body: '', completed: false };
 
     constructor(private todoListService: TodoListService) { }
 
     ngOnInit(): void {
+        // Fetch todo list
         this.todoListService.getTodoList()
             .subscribe((resp: ToDo[]) => {
                 this.completedTasks = resp.filter((todo) => todo.completed);
@@ -22,5 +28,70 @@ export class AppComponent implements OnInit {
                 console.log(this.completedTasks);
                 console.log(this.todoTasks);
             });
+    }
+
+    deleteTodo(task: ToDo): void {
+        console.log(`I'm removing task with id: ${task.id}`)
+
+        // Update DOM
+        if (task.completed) {
+            this.completedTasks = this.completedTasks.filter((todo) => todo.id !== task.id);
+            console.log(this.completedTasks);
+        } else {
+            this.todoTasks = this.todoTasks.filter((todo) => todo.id !== task.id);
+            console.log(this.todoTasks);
+        }
+
+        // Remove at DB
+
+    }
+
+    addTodo(): void {
+        if (this.newTodoBody.trim().length === 0) return;
+
+        // Todo to add
+        const newTodo: ToDo = { id: '6', body: this.newTodoBody, completed: false };
+        // Update the DOM
+        this.todoTasks.push(newTodo);
+        // Add in database
+        this.todoListService.addTodo(newTodo)
+            .subscribe((resp) => console.log(resp));
+        // Clean input
+        this.newTodoBody = '';
+    }
+
+    toggleCompletness(task: ToDo): void {
+        // update the DOM
+        if (task.completed) {
+            // Remove from completed tasks
+            this.completedTasks = this.completedTasks.filter((todo) => todo.id !== task.id);
+            // Add to uncompleted tasks
+            this.todoTasks.push(task);
+        } else {
+            // Remove from uncompleted tasks
+            this.todoTasks = this.todoTasks.filter((todo) => todo.id !== task.id);
+            // Add to completed tasks
+            this.completedTasks.push(task);
+        }
+
+        // Toggle 'completed' property
+        task.completed = !task.completed;
+    }
+
+    openModalUpdateTask(task: ToDo) {
+        this.editedTask = {...task};
+    }
+
+    updateTask() {
+        // Update DOM
+        let task;
+        if(this.editedTask.completed) {
+            task = this.completedTasks.find((todo) => todo.id === this.editedTask.id);
+        } else {
+            task = this.todoTasks.find((todo) => todo.id === this.editedTask.id);
+        }
+        task!.body = this.editedTask.body;
+
+        // Update at DB
     }
 }
--- src/app/services/todo-list.service.ts
+++ src/app/services/todo-list.service.ts
@@ -7,35 +7,44 @@ import { ToDo } from "../interfaces/to-do.interface";
 })
 export class TodoListService {
 
+    list: ToDo[] = [
+        {
+            id: '1',
+            body: 'Go out for a walk',
+            completed: true,
+        },
+        {
+            id: '2',
+            body: 'Read p. 144',
+            completed: true,
+        },
+        {
+            id: '3',
+            body: 'Do the laundry',
+            completed: false,
+        },
+        {
+            id: '4',
+            body: 'Tidy the room',
+            completed: false,
+        },
+        {
+            id: '5',
+            body: 'Study for math exam',
+            completed: false,
+        },
+    ]
+
     constructor() { }
 
     getTodoList(): Observable<ToDo[]> {
-        return new Observable((observer) => observer.next([
-            {
-                id: '1',
-                body: 'Go out for a walk',
-                completed: true,
-            },
-            {
-                id: '2',
-                body: 'Read p. 144',
-                completed: true,
-            },
-            {
-                id: '3',
-                body: 'Do the laundry',
-                completed: false,
-            },
-            {
-                id: '4',
-                body: 'Tidy the room',
-                completed: false,
-            },
-            {
-                id: '5',
-                body: 'Study for math exam',
-                completed: false,
-            },
-        ]));
+        return new Observable((observer) => observer.next(this.list));
+    }
+
+    addTodo(task: ToDo): Observable<any> {
+        return new Observable((observer) => {
+            this.list.push(task);
+            observer.next(task);
+        });
     }
 }
